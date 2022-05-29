1e192d51dbcd983314ce224b70ad632da8f26e95
Kang Minji <mminjg@gmail.com>
2022-05-29 18:04:13 +0900

    ✨ svg 컬러 조정

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { ColorCode } from '../utils/constants';
+
+import Love from '../emotions/Love';
 
 const SettingsScreen = () => {
   return (
     <View>
       <Text>settings</Text>
+      <Love width={120} height={40} color={ColorCode.PINK} />
     </View>
   );
 };
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,8 @@
 const ColorCode = {
   PRIMARY: '#8E97FD',
   GRAY1: '#8F9BB3',
+  WHITE: '#FFFFFF',
+  PINK: '#FDC4DC',
 };
 
 export { ColorCode };
