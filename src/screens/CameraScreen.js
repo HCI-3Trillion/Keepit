5fa4730b5b8575145c8678d54da03088a65d0c6f
dooli1971039 <dooli9819@gmail.com>
2022-05-31 16:18:07 +0900

    💄 카메라 UI 수정

--- src/screens/CameraScreen.js
+++ src/screens/CameraScreen.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { Camera } from 'expo-camera';
-
+import { MaterialIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 const CameraScreen = ({ navigation }) => {
   const [hasPermission, setHasPermission] = useState(null);
   const [camera, setCamera] = useState(null);
@@ -24,6 +25,20 @@ const CameraScreen = ({ navigation }) => {
     }
   };
 
+  const pressFlip = () => {
+    setType(
+      type === Camera.Constants.Type.back ? Camera.Constants.Type.front : Camera.Constants.Type.back
+    );
+  };
+
+  const pressFlash = () => {
+    setType(
+      type === Camera.Constants.FlashMode.off
+        ? Camera.Constants.FlashMode.on
+        : Camera.Constants.FlashMode.off
+    );
+  };
+
   if (hasPermission === null) {
     return <View />;
   }
@@ -34,22 +49,26 @@ const CameraScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <View style={styles.cameraContainer}>
-        <Camera style={styles.camera} ref={(ref) => setCamera(ref)} type={type} ratio={'1:1'} />
+        <Camera style={styles.camera} ref={(ref) => setCamera(ref)} type={type} />
       </View>
+
       <View style={styles.buttonContainer}>
-        <Button
-          style={styles.button}
-          onPress={() => {
-            setType(
-              type === Camera.Constants.Type.back
-                ? Camera.Constants.Type.front
-                : Camera.Constants.Type.back
-            );
-          }}
-          title="Flip"
-        />
-        <Button style={styles.button} onPress={() => takePicture()} title="Take Picture" />
+        {/* 플래쉬 */}
+        <TouchableOpacity style={styles.buttonFlash} onPress={pressFlash}>
+          <MaterialIcons name="flash-off" size={40} color="white" />
+        </TouchableOpacity>
+
+        {/* 사진 찍기 */}
+        <TouchableOpacity style={styles.buttonTakePicture} onPress={takePicture}>
+          <MaterialIcons name="camera" size={90} color="white" />
+        </TouchableOpacity>
+
+        {/* 화면 전환 */}
+        <TouchableOpacity style={styles.buttonFlip} onPress={pressFlip}>
+          <MaterialCommunityIcons name="rotate-3d-variant" size={40} color="white" />
+        </TouchableOpacity>
       </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -58,19 +77,32 @@ const CameraScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'black',
   },
   cameraContainer: {
-    flex: 1,
+    flex: 8,
     flexDirection: 'row',
   },
   camera: {
     flex: 1,
-    aspectRatio: 1,
   },
   buttonContainer: {
-    flex: 1,
+    flex: 2,
+    flexDirection: 'row',
+    alignItems: 'center',
   },
-  button: {},
+  buttonFlash: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  buttonTakePicture: {
+    flex: 2,
+    alignItems: 'center',
+  },
+  buttonFlip: {
+    flex: 1,
+    alignItems: 'center',
+  },
 });
 
 export default CameraScreen;
